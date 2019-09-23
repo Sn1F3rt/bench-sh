7f2ce72a3af9ef5e5c7b6fab59e65c83d8291878
Sayan Bhattacharyya <sohamb03@outlook.com>
2019-09-23 15:37:00 +0530

    Update bench.sh

--- bench.sh
+++ bench.sh
@@ -80,8 +80,8 @@ speedtest4 () {
 	echo "Seattle, WA, US		Softlayer	$slwa " | tee -a $HOME/bench.log
 	slsjc=$( wget -4 -O /dev/null http://speedtest.sjc01.softlayer.com/downloads/test100.zip 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}' )
 	echo "San Jose, CA, US	Softlayer	$slsjc " | tee -a $HOME/bench.log
-	linodemum=$( wget -4 -O /dev/null http://speedtest.mumbai1.linode.com/100MB-mumbai.bin 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}' )
-	echo "Mumbai, MH, IN        Linode 	$linodemum " | tee -a $HOME/bench.log
+	slhou=$( wget -4 -O /dev/null http://speedtest.hou2.softlayer.com/downloads/test100.zip 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}' )
+	echo "Houston, US       Softlayer	$slhou " | tee -a $HOME/bench.log
 	echo "" | tee -a $HOME/bench.log
 	# Asia speed test
 	linodejp=$( wget -4 -O /dev/null http://speedtest.tokyo2.linode.com/100MB-tokyo2.bin 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}' )
